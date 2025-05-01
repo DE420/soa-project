458988e9ade668f53bd40821f92ed7c1f1dcab7f
happyqq833 <happyqq2003@gmail.com>
2025-05-01 21:55:28 +0700

    fix kafka connector

--- register-connectors.sh
+++ register-connectors.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
-http PUT http://localhost:8083/connectors/reservation-outbox-connector/config < reservation-service/reservation-outbox-connector.json
-http PUT http://localhost:8083/connectors/hotel-outbox-connector/config < hotel-service/hotel-outbox-connector.json
-http PUT http://localhost:8083/connectors/payment-outbox-connector/config < payment-service/payment-outbox-connector.json
+curl -X PUT -H "Content-Type: application/json" \
+  --data @reservation-service/reservation-outbox-connector.json \
+  http://localhost:8083/connectors/reservation-outbox-connector/config
+
+curl -X PUT -H "Content-Type: application/json" \
+  --data @hotel-service/hotel-outbox-connector.json \
+  http://localhost:8083/connectors/hotel-outbox-connector/config
+
+curl -X PUT -H "Content-Type: application/json" \
+  --data @payment-service/payment-outbox-connector.json \
+  http://localhost:8083/connectors/payment-outbox-connector/config
